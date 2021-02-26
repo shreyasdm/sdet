22205e156b62579bd056775ba426ade5ce2a17b7
shreyasdm <43364799+shreyasdm@users.noreply.github.com>
2021-02-26 11:24:44 +0530

    Add files via upload

---
+++ python/Activity9.py
@@ -0,0 +1,30 @@
+# Given lists
+#listOne = [10, 20, 23, 11, 17]
+#listTwo = [13, 43, 24, 36, 12]
+
+# get the list from user
+listOne = list(input("Enter the first list of numbers: ").split(", "))
+listTwo= list(input("Enter the second list of numbers: ").split(", "))
+
+
+# Print the lists
+print("First List ", listOne)
+print("Second List ", listTwo)
+
+# Declare a third list that will contain the result
+thirdList =[]
+
+# Iterate through first list to get odd elements
+for num in listOne:
+    print(num)
+    if (int(num) % 2 != 0):
+        thirdList.append(num)
+        
+# Iterate through first list to get even elements
+for num in listTwo:
+    if (int(num) % 2 == 0):
+        thirdList.append(num)
+
+# Print result
+print("result List is:")
+print(thirdList)
