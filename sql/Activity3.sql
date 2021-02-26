fc021a9014d216658b9749f8ac6e2b104f8c29b6
shreyasdm <43364799+shreyasdm@users.noreply.github.com>
2021-02-26 11:28:15 +0530

    Add files via upload

---
+++ sql/Activity3.sql
@@ -0,0 +1,8 @@
+-- Show data from the salesman_id and city columns
+SELECT salesman_id, city FROM salesman;
+
+-- Show data of salesman from Paris
+SELECT * FROM salesman WHERE city='Paris';
+
+-- Show salesman_id and commission of Paul Adam
+SELECT salesman_id, commission FROM salesman WHERE name='Paul Adam';
